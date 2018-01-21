4d4b29871583312db45eb58d9b1cc113e686329a
Guillaume Tran <guillaume.tran@epitech.eu>
2018-01-22 00:05:04 +0100

    Initialize first workshop

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Final from "./src/Final/Final";
+import Workshop from "./src/Workshop-1/Workshop";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Final />
+        <Workshop />
       </View>
     );
   }
---
+++ src/Workshop-1/Workshop.js
@@ -0,0 +1,19 @@
+/**
+ * Created by guillaumetran on 22/01/2018.
+ */
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class Workshop extends React.Component {
+  state = {};
+
+  render() {
+    return <View style={styles.container} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
